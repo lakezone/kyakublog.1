a1eabad5de20cb2b716622602fcf72896e88b148
Prelang Builder <builder@prelang.com>
2015-07-19 05:59:46 +0000

    Generated scaffolding for model 'Article' via Rails

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :post, :user_id, :user_id, :id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+
   resources :users
 
   devise_for :admin_users, ActiveAdmin::Devise.config
